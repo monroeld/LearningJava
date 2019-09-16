cdad66ff87d6fe50f2c49816c574398aae8b82ac
Luke Monroe <ldmonroe80@gmail.com>
2019-09-16 11:28:39 -0700

    No progress yet
    The new files are in the drive.  Gonna have to do some kludging to get it all together...

--- SSASF_Calc.R
+++ SSASF_Calc.R
@@ -2,15 +2,25 @@ require(ggplot2)
 require(dplyr)
 require(plyr)
 require(RColorBrewer)
+library(tidyverse)
 
 data <- read.csv("Bursaries_2013-Present.csv", sep = ",", stringsAsFactors = FALSE)
-data <- data[, 2:4]
+data <- data[, 3:5]
+
+colnames(data) <- c("Scholar", "Date", "ZAR")
+
+test <- data %>% mutate(Date, as.Date(Date, format = "%Y/%m"))
 
 data$DATE.RECEIVED <- as.Date(data$DATE.RECEIVED, format = "%d/%m/%Y")
+data <- 
 
 colnames(data) <- c("Recipient", "Date", "Disbursement")
 
 data$Disbursement <- gsub(",", "", data$Disbursement)
+
+# 
+
+
 
 data <- do.call(rbind, lapply(unique(data$Recipient), function(x) {
         working <- subset(data, Recipient == x)
